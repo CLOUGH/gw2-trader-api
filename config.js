f2e285dcc78a3de9bdfddc7587416ff8fad80a2b
Warren Clough <clough.warren@gmail.com>
2017-11-26 19:31:11 -0500

    inital commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  environment: process.env.NODE_ENV || 'dev',
+  server: {
+    port: process.env.PORT || 8080
+  },
+  mongo: {
+    url: process.env.MONGO_DB_URI || 'mongodb://localhost/gw2-trader-api'
+  }
+};
+
+module.exports = config;
