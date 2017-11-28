c19662060110fc968f2ffe8db3db2d376c78ade1
Warren Clough <clough.warren@gmail.com>
2017-11-27 22:43:00 -0500

    added apis and pull data

--- index.js
+++ index.js
@@ -4,35 +4,52 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const bluebird = require('bluebird');
+const cors = require('cors');
+let io = require('socket.io');
+const http = require('http');
 
 const config = require('./config');
 const routes = require('./routes');
 
 const app = express();
+const server = http.createServer(app);
 
 mongoose.Promise = bluebird;
 mongoose.connect(config.mongo.url, {
   useMongoClient: true
 });
 
+io = io(server);
 
+app.use(cors());
 app.use(helmet());
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.use('/', routes);
-
-const server = app.listen(config.server.port, () => {
-  console.log(`Magic happens on port ${config.server.port}`);
+// adding socket io through the middleware
+app.use((req, res, next) => {
+  req.io = io;
+  next();
 });
-
-const io = require('socket.io').listen(server);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.emit('connected');
+
+  io.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-module.exports = app;
+app.use('/', routes);
+
+server.listen(config.server.port, () => {
+  console.log(`Magic happens on port ${config.server.port}`);
+});
+
+module.exports = server;
--- model/item/controller.js
+++ model/item/controller.js
@@ -1,21 +1,75 @@
 const Controller = require('../../lib/controller');
 const itemFacade = require('./facade');
 const axios = require('axios');
+const TaskQueue = require('cwait').TaskQueue;
+const Promise = require('bluebird');
+const _ = require('lodash');
 
 class ItemController extends Controller {
 
   constructor(facade) {
     super(facade);
     this.gw2ApiUrl = 'https://api.guildwars2.com/v2';
+    this.gw2Spidy = 'http://www.gw2spidy.com/api/v0.9/json';
+    this.gw2Shinies = 'https://www.gw2shinies.com/api/json/';
+    this.MAX_SIMULTANEOUS_DOWNLOADS = 100;
+    this.itemsPerRequest = 200;
   }
 
   syncItems(req, res, next) {
-    console.log('rest', req.io);
-    req.io.emit('downloading item ids');
-    // items
-    this.getItemIds(next).then((ids) => {
-      console.log(ids);
-      req.io.emit('item ids downloaded');
+    req.io.emit('/items/sync', {
+      message: 'Getting item ids'
+    });
+
+    // get items
+    this.getItemIds(next).then((response) => {
+      const ids = response.data;
+      const numberOfIds = ids.length;
+
+      req.io.emit('/items/sync', {
+        message: `Got ${numberOfIds} items`
+      });
+
+      const idChunks = ItemController.chunk(response.data, this.itemsPerRequest);
+      let itemsDownloaded = 0;
+      const queue = new TaskQueue(Promise, this.MAX_SIMULTANEOUS_DOWNLOADS);
+
+      req.io.emit('/items/sync', {
+        message: 'Building urls'
+      });
+
+
+      const promise = Promise.map(idChunks, queue.wrap(idChunk =>
+        axios.get(`${this.gw2ApiUrl}/items?ids=${idChunk}`)
+          .then((itemResponse) => {
+            itemsDownloaded += itemResponse.data.length;
+            req.io.emit('/items/sync', {
+              message: `Downloaded ${itemsDownloaded} items of ${numberOfIds}`
+            });
+
+            const items = itemResponse.data;
+            const tradingHistoryQueue = new TaskQueue(Promise, this.MAX_SIMULTANEOUS_DOWNLOADS);
+
+            return Promise.map(items, tradingHistoryQueue.wrap(item =>
+              axios.get(`${this.gw2Shinies}/history-daily/${item.id}`)
+                .then((historyResponse) => {
+                  item.history = historyResponse.data;
+                  return item;
+                })
+                .catch(err => next(err))
+            ));
+          })
+          .catch(err => next(err))
+      ));
+
+
+      promise.then((values) => {
+        console.log(values);
+
+        req.io.emit('/items/sync', {
+          message: 'Done'
+        });
+      }).catch(err => next(err));
     });
 
     return res.json([]);
@@ -25,6 +79,17 @@ class ItemController extends Controller {
     return axios.get(`${this.gw2ApiUrl}/items`)
       .catch(err => next(err));
   }
+
+  static chunk(array, size) {
+    const arrays = [];
+
+    while (array.length > 0) {
+      arrays.push(array.splice(0, size));
+
+    }
+    return arrays;
+  }
+
 }
 
 module.exports = new ItemController(itemFacade);
