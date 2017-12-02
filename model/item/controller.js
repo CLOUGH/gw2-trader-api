346d62250578a4b453ea20fa96745fbf86dba10b
Warren Clough <clough.warren@gmail.com>
2017-12-02 18:12:13 -0500

    added filters

--- model/item/controller.js
+++ model/item/controller.js
@@ -26,10 +26,132 @@ class ItemController extends Controller {
     const sortBy = {
       'listings.roi': -1,
     };
-    // sortBy[sort] = -1;
+
+    const filter = {
+      name: {
+        $regex: req.query.name || '',
+        $options: 'i'
+      },
+      type: {
+        $regex: req.query.type || '',
+        $options: 'i'
+      },
+      rarity: {
+        $regex: req.query.rarity || '',
+        $options: 'i'
+      }
+    };
+    // Level
+    if (req.query.minLevel) {
+      filter.level = Object.assign({
+        $gte: Number(req.query.minLevel)
+      }, filter.level);
+    }
+    if (req.query.maxLevel) {
+      filter.level = Object.assign({
+        $lte: Number(req.query.maxLevel)
+      }, filter.level);
+    }
+    // Buy
+    if (req.query.minBuy) {
+      filter.buy = Object.assign({
+        $gte: Number(req.query.minBuy)
+      }, filter.buy);
+    }
+    if (req.query.maxBuy) {
+      filter.buy = Object.assign({
+        $lte: Number(req.query.maxBuy)
+      }, filter.buy);
+    }
+
+    // sell
+    if (req.query.minSell) {
+      filter.sell = Object.assign({
+        $gte: Number(req.query.minSell)
+      }, filter.sell);
+    }
+    if (req.query.maxSell) {
+      filter.sell = Object.assign({
+        $lte: Number(req.query.maxSell)
+      }, filter.sell);
+    }
+
+    // demand
+    if (req.query.minDemand) {
+      filter.demand = Object.assign({
+        $gte: Number(req.query.minDemand)
+      }, filter.demand);
+    }
+    if (req.query.maxDemand) {
+      filter.demand = Object.assign({
+        $lte: Number(req.query.maxDemand)
+      }, filter.demand);
+    }
+
+    // supply
+    if (req.query.minSupply) {
+      filter.supply = Object.assign({
+        $gte: Number(req.query.minSupply)
+      }, filter.supply);
+    }
+    if (req.query.maxSupply) {
+      filter.supply = Object.assign({
+        $lte: Number(req.query.maxSupply)
+      }, filter.supply);
+    }
+
+    // profit
+    if (req.query.minProfit) {
+      filter.profit = Object.assign({
+        $gte: Number(req.query.minProfit)
+      }, filter.profit);
+    }
+    if (req.query.maxProfit) {
+      filter.profit = Object.assign({
+        $lte: Number(req.query.maxProfit)
+      }, filter.profit);
+    }
+
+    // roi
+    if (req.query.minROI) {
+      filter.roi = Object.assign({
+        $gte: Number(req.query.minROI)
+      }, filter.roi);
+    }
+    if (req.query.maxROI) {
+      filter.roi = Object.assign({
+        $lte: Number(req.query.maxROI)
+      }, filter.roi);
+    }
+
+    // buc
+    if (req.query.minBUC) {
+      filter.buc = Object.assign({
+        $gte: Number(req.query.minBUC)
+      }, filter.buc);
+    }
+    if (req.query.maxBUC) {
+      filter.buc = Object.assign({
+        $lte: Number(req.query.maxBUC)
+      }, filter.buc);
+    }
+
+    // suc
+    if (req.query.minSUC) {
+      filter.suc = Object.assign({
+        $gte: Number(req.query.minSUC)
+      }, filter.suc);
+    }
+    if (req.query.maxSUC) {
+      filter.suc = Object.assign({
+        $lte: Number(req.query.maxSUC)
+      }, filter.suc);
+    }
+
+    console.log(filter);
 
     return this.facade.model
-      .find({}, {
+      .find(filter, {
         id: 1,
         name: 1,
         icon: 1,
